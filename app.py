a9c1ece25816a973302e1165d279c1a1e7fb4101
bsearight <benjamin.searight@western.edu>
2025-03-24 15:30:20 -0600

    Database refactored

--- app.py
+++ app.py
@@ -9,77 +9,80 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.init_app(app)
 
-class LoginInfo(db.Model, UserMixin):
-    __tablename__ = 'login_info'
-    loginid = db.Column(db.Integer, primary_key=True)
+@app.context_processor
+def inject_user():
+    return dict(user=current_user)
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    last_login = db.Column(db.DateTime)
-    is_active = db.Column(db.Boolean, default=True)
-    
-    user_profile = db.relationship("UserProfile", backref="login_info", uselist=False)
-
-class UserProfile(db.Model):
-    __tablename__ = 'user_profile'
-    UserID = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(255), nullable=False)
-    last_name = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    prefered_medium = db.Column(db.String(255))
-    loginid = db.Column(db.Integer, db.ForeignKey('login_info.loginid'), unique=True)
-    
-    projects = db.relationship("Project", backref="user_profile")
-    supplies = db.relationship("Supply", backref="user_profile")
+    projects = db.relationship("Project", backref="user")
+    supplies = db.relationship("Supply", backref="user")
 
 class Project(db.Model):
     __tablename__ = 'project'
-    ProjectID = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text)
     image = db.Column(db.String(255))
-    field = db.Column(db.String(255))
     date = db.Column(db.Date)
     notes = db.Column(db.Text)
     status = db.Column(db.String(255))
-    UserID = db.Column(db.Integer, db.ForeignKey('user_profile.UserID'))
-    
-    project_notes = db.relationship("ProjectNotes", backref="project")
+    owner = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class Supply(db.Model):
     __tablename__ = 'supply'
-    SupplyID = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
     brand = db.Column(db.String(255))
-    category = db.Column(db.String(255))
     purchase_link = db.Column(db.String(255))
     cost = db.Column(db.Numeric(10, 2))
     rating = db.Column(db.Numeric(3, 2))
-    information = db.Column(db.Text)
     notes = db.Column(db.Text)
-    description = db.Column(db.Text)
-    UserID = db.Column(db.Integer, db.ForeignKey('user_profile.UserID'))
-    
-    supply_notes = db.relationship("SupplyNotes", backref="supply")
+    image = db.Column(db.String(255))
+    owner = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-class SupplyNotes(db.Model):
-    __tablename__ = 'supply_notes'
-    snoteid = db.Column(db.Integer, primary_key=True)
-    note = db.Column(db.Text, nullable=False)
-    date = db.Column(db.Date)
-    importance = db.Column(db.Integer)
-    SupplyID = db.Column(db.Integer, db.ForeignKey('supply.SupplyID'))
-
-class ProjectNotes(db.Model):
-    __tablename__ = 'project_notes'
-    pnoteid = db.Column(db.Integer, primary_key=True)
-    note = db.Column(db.Text, nullable=False)
-    date = db.Column(db.Date)
-    importance = db.Column(db.Integer)
-    ProjectID = db.Column(db.Integer, db.ForeignKey('project.ProjectID'))
+def database_insert(data):
+    if isinstance(data, User):
+        db.session.add(data)
+    elif isinstance(data, Project):
+        db.session.add(data)
+    elif isinstance(data, Supply):
+        db.session.add(data)
+    db.session.commit()
+def database_update(data):
+    if isinstance(data, User):
+        db.session.merge(data)
+    elif isinstance(data, Project):
+        db.session.merge(data)
+    elif isinstance(data, Supply):
+        db.session.merge(data)
+    db.session.commit()
+def database_getProjects():
+    projects = Project.query.all()
+    return projects
+def database_getSupplies():
+    supplies = Supply.query.all()
+    return supplies
+def database_getProject(project_id):
+    project = db.session.get(Project, project_id)
+    if project:
+        return project
+    else:
+        return None
+def database_getSupply(supply_id):
+    supply = db.session.get(Supply, supply_id)
+    if supply:
+        return supply
+    else:
+        return None
 
 @login_manager.user_loader
 def load_user(user_id):
-    return LoginInfo.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
 
 @app.route("/")
 def home():
@@ -95,10 +98,13 @@ def view_project():
 
 @app.route("/view_project/<project_id>")
 def view_project_id(project_id):
+    project = database_getProject(project_id)
+    if not project:
+        return redirect("/projects")
     project_details = {
-        "id": project_id,
-        "name": f"Project {project_id}",
-        "description": f"This is the description for project {project_id}."
+        "id": project.project_id,
+        "name": project.title,
+        "description": project.description
     }
     return render_template("view_project.html", project_details=project_details)
 
@@ -110,14 +116,13 @@ def create_project():
 def create_project_submit():
     name = request.form["project_name"]
     description = request.form["project_description"]
-    new_project = Project(title=name, description=description)
-    db.session.add(new_project)
-    db.session.commit()
+    database_insert(Project(title=name, description=description))
     return redirect("/projects")
 
 @app.route("/items")
 def items():
-    return render_template("items.html")
+    supplies = database_getSupplies()
+    return render_template("items.html", supplies=supplies)
 
 @app.route("/view_item")
 def view_item():
@@ -125,10 +130,13 @@ def view_item():
 
 @app.route("/view_item/<item_id>")
 def view_item_id(item_id):
+    supply = database_getSupply(item_id)
+    if not supply:
+        return redirect("/items")
     item_details = {
-        "id": item_id,
-        "name": f"Item {item_id}",
-        "description": f"This is the description for item {item_id}."
+        "id": supply.supply_id,
+        "name": supply.name,
+        "description": supply.description
     }
     return render_template("view_item.html", item_details=item_details)
 
@@ -137,7 +145,7 @@ def register():
     ## WARNING: THIS IS NOT SECURED. DO NOT USE IN PRODUCTION
     if request.method == "POST":
         username = request.form["username"]
-        user = LoginInfo.query.filter_by(username=username).first()
+        user = User.query.filter_by(username=username).first()
         if user:
             return render_template("register.html")
         else:
@@ -145,11 +153,30 @@ def register():
             cpassword = request.form["confirm"]
             if password != cpassword:
                 return redirect("/")
-            user = LoginInfo(username=username, password=password)
-            db.session.add(user)
-            db.session.commit()
+            database_insert(User(username=username, password=password))
             return redirect("/")
     return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    ## WARNING: THIS IS NOT SECURED. DO NOT USE IN PRODUCTION
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = User.query.filter_by(username=username).first()
+        if user and password == user.password:
+            login_user(user)
+            print("logged in user:", user.username)
+        else:
+            return render_template("login.html")
+        return redirect("/")
+    return render_template("login.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 @app.route("/create_item")
 def create_item():
@@ -159,9 +186,7 @@ def create_item():
 def create_item_submit():
     name = request.form["item_name"]
     description = request.form["item_description"]
-    new_supply = Supply(name=name, description=description)
-    db.session.add(new_supply)
-    db.session.commit()
+    database_insert(Supply(name=name, description=description))
     return redirect("/items")
 
 with app.app_context():
